ae01bd837518b7edd3fc1c8e09834650b710e622
Vladimir H <vladimiroski@gmail.com>
2008-12-22 10:37:24 -0600

    Identación de Modulos principales.

--- modulos/Digicel.php
+++ modulos/Digicel.php
@@ -1,20 +1,20 @@
 <?php
-//DIGICEL, S.A. de C.V.
+  //DIGICEL, S.A. de C.V.
 
 function Digicel_Nombre() {
-  return "Digicel:Live";
-}
+    return "Digicel:Live";
+  }
 
 function Digicel_Enviar($telefono,$mensaje,$firma) {
-$headers = 'From: digicel@digimensajes.com' . "\r\n" .
-    'Reply-To: digicel@digimensajes.com' . "\r\n";
-$to = "503".$telefono."@digimensajes.com";
-$subject = $firma;
-$body = $mensaje;
-if (mail($to, $subject, $body, $headers)) {
-  return true;
- } else {
-  return false;
- }
+    $headers = 'From: digicel@digimensajes.com' . "\r\n" .
+        'Reply-To: digicel@digimensajes.com' . "\r\n";
+    $to = "503".$telefono."@digimensajes.com";
+    $subject = $firma;
+    $body = $mensaje;
+    if (mail($to, $subject, $body, $headers)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 ?>
--- modulos/Telecom.php
+++ modulos/Telecom.php
@@ -1,19 +1,19 @@
 <?php
 function Telecom_Nombre()
 {
-return "Telecom:Claro/Personal";
+    return "Telecom:Claro/Personal";
 } 
 function Telecom_Enviar($telefono, $mensaje, $firma)
 {
-$headers = 'From: noticlaro@sms.claro.com.sv' . "\r\n" .
-    'Reply-To: noticlaro@sms.claro.com.sv' . "\r\n";
-$to = $telefono."@sms.claro.com.sv";
-$subject = $firma;
-$body = $mensaje;
-if (mail($to, $subject, $body, $headers)) {
-  return true;
- } else {
-  return false;
- }
+    $headers = 'From: noticlaro@sms.claro.com.sv' . "\r\n" .
+        'Reply-To: noticlaro@sms.claro.com.sv' . "\r\n";
+    $to = $telefono."@sms.claro.com.sv";
+    $subject = $firma;
+    $body = $mensaje;
+    if (mail($to, $subject, $body, $headers)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 ?>
