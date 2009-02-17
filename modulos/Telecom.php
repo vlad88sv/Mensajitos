9a746639012b53435c60bd7c9a4ce2e34a2eca44
Vladimir Hidalgo <vlad@aurox.homeip.net>
2009-02-16 23:44:10 -0600

    Nuevos rangos a Telecom: 2103-0000 - 2104-4999 -> Telecom Pequeñas correciones en el envío a Telecom

--- modulos/Telecom.php
+++ modulos/Telecom.php
@@ -5,15 +5,10 @@ function Telecom_Nombre()
 } 
 function Telecom_Enviar($telefono, $mensaje, $firma)
 {
-	$headers  = "To: $telefono<$telefono@sms.claro.com.sv>" . "\r\n";
-	$headers .= "From: My noticlaro<noticlaro@sms.claro.com.sv>" . "\r\n";
-    $headers .= "Reply-To: sms@todosv.com" . "\r\n";
-    $headers .= "Return-Path: sms@todosv.com" . "\r\n";
-    $headers .= "X-Mailer: mensajitos.php" . "\r\n";
-    $headers .= "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-
-    $to = $telefono."@sms.claro.com.sv" . "\r\n";
+    $headers = 'From: noticlaro@sms.claro.com.sv' . "\n" .
+        'Reply-To: noticlaro@sms.claro.com.sv' . "\n";
+    $to = $telefono."@sms.claro.com.sv";
+	//$to "vladimiroski@gmail.com";
     $subject = $firma;
     $body = $mensaje;
     if (mail($to, $subject, $body, $headers)) {
