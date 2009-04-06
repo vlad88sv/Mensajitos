d18c57cc31a146b9760023478952942312a8e14d
Vladimir Hidalgo <vlad@todosv.com>
2009-04-05 20:38:53 -0600

    Soporte beta para telefonica.

--- modulos/Telefonica.php
+++ modulos/Telefonica.php
@@ -1,11 +1,11 @@
 <?php
-function Telefonica_Nombre() 
+function Telefonica_Nombre()
 {
     return "Telefonica:Movistar";
 }
 function Telefonica_Enviar($telefono,$mensaje,$firma) {
     $snoopy = new Snoopy;
-    $comando="http://aurox.sytes.net/telefonica/enviar.php?fir=".rawurlencode($firma)."&tel=".$telefono."&men=".rawurlencode($mensaje);
+    $comando="http://aurox.sytes.net/telefonica/enviar.php?firma=".rawurlencode($firma)."&telefono=".$telefono."&mensaje=".rawurlencode($mensaje);
     $snoopy->fetch($comando);
     //Evaluar la salida
     if($snoopy->results == '0'){
